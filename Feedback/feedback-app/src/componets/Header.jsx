9c4b707bcc35bca60506b1b7f17a0e6d8116adeb
Jaylen Carroll <jaylencarroll87@gmail.com>
2022-03-29 15:19:19 -0400

    Bugfix

--- Feedback/feedback-app/src/componets/Header.jsx
+++ Feedback/feedback-app/src/componets/Header.jsx
@@ -1,7 +1,5 @@
 import PropTypes from "prop-types"
 
-//Header has constant prop, Props are argments passes into React Componetns
-//Props are passes to components via HTML attributes. They are like funtion arguments in JS and attributes in HTML
 function Header({ text, bgColor, textColor }) {
   const headerStyles = {
     backgroundColor: bgColor,
@@ -16,16 +14,17 @@ function Header({ text, bgColor, textColor }) {
     </header>
   )
 }
-// The name of the component this is a prop
+
 Header.defaultProps = {
   text: "Feedback UI",
   bgColor: "rgba(0,0,0,0.4)",
   textColor: "#ff6a95",
 }
 
-//This is for extra valiation you can set the prop type to be specific string,boolean
 Header.propTypes = {
   text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
 }
 
 export default Header
